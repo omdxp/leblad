b317dd196e0a4093201091683199e241e40a3ffb
omdxp <omarbelghaouti@gmail.com>
2022-10-21 23:12:46 +0100

    feat: define `WilayaByZipCodeError` struct

--- error.go
+++ error.go
@@ -7,3 +7,11 @@ type WilayaListError struct{}
 func (e *WilayaListError) Error() string {
 	return "error getting wilaya list"
 }
+
+// WilayaByZipCodeError is the error returned when getting a wilaya by zip code
+type WilayaByZipCodeError struct{}
+
+// Error returns the error message
+func (e *WilayaByZipCodeError) Error() string {
+	return "error getting wilaya by zip code"
+}
