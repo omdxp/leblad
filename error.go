fe39c1e6afd93aba955da1a1751a3b2815e21530
omdxp <omarbelghaouti@gmail.com>
2022-10-21 23:09:10 +0100

    feat: define `WilayaListError` struct

---
+++ error.go
@@ -0,0 +1,9 @@
+package leblad
+
+// WilayaListError is the error returned when getting the wilaya list
+type WilayaListError struct{}
+
+// Error returns the error message
+func (e *WilayaListError) Error() string {
+	return "error getting wilaya list"
+}
