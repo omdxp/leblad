86bf7694121e8ac50efa7e420298825ce598dddd
omdxp <omarbelghaouti@gmail.com>
2022-10-23 22:44:30 +0100

    feat: unmarshal `data/WilayaList.json` file

--- utils.go
+++ utils.go
@@ -1,6 +1,9 @@
 package leblad
 
-import "os"
+import (
+	"encoding/json"
+	"os"
+)
 
 // openJsonFile opens a local json file with the given name
 func openJsonFile(n string) ([]byte, error) {
@@ -9,4 +12,13 @@ func openJsonFile(n string) ([]byte, error) {
 		return nil, err
 	}
 	return bytes, nil
+}
+
+// unmarshalWilayaListJson unmarshals the given json bytes into a slice of wilayas
+func unmarshalWilayaListJson(b []byte) (*[]Wilaya, error) {
+	var wilayas *[]Wilaya
+	if err := json.Unmarshal(b, &wilayas); err != nil {
+		return nil, err
+	}
+	return wilayas, nil
 }
--- utils_test.go
+++ utils_test.go
@@ -24,3 +24,25 @@ func Test_openJsonFile(t *testing.T) {
 		})
 	}
 }
+
+func Test_unmarshalWilayaListJson(t *testing.T) {
+	tests := []struct {
+		name    string
+		wantErr bool
+	}{
+		{
+			name:    "data/WilayaList.json",
+			wantErr: false,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			b, _ := openJsonFile(tt.name)
+			_, err := unmarshalWilayaListJson(b)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("unmarshalWilayaListJson() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+		})
+	}
+}
